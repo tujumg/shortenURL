17ff21545f1bfa63e7db44f9ede0f5de0e4b1dce
tujumg <tujumg@gmail.com>
2022-05-27 16:52:37 +0800

    add models folder

---
+++ models/URL.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const URLSchema = new Schema({
+    originalURL:{type:String,required:true},
+    shortURL:{type:String,required:true}
+})
+module.exports = mongoose.model('URL',URLSchema)
